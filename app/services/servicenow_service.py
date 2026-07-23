f3634f5940d6ce35903672cfd04dd844605fcb8b
Chappa Bharath kumar <chappabharathkumar8@gmail.com>
2026-07-23 12:10:00 +0530

    feat: add find_existing_incident query parameters template

--- app/services/servicenow_service.py
+++ app/services/servicenow_service.py
@@ -5,3 +5,22 @@ from app.core.config import (
     SERVICENOW_USERNAME,
     SERVICENOW_PASSWORD,
 )
+
+def find_existing_incident(website_url: str):
+    url = f"{SERVICENOW_INSTANCE}/api/now/table/incident"
+
+    headers = {
+        "Accept": "application/json"
+    }
+
+    params = {
+        "sysparm_query": (
+            f"active=true"
+            f"^short_description=Website Down"
+            f"^descriptionLIKE{website_url}"
+        ),
+        "sysparm_fields": "number,sys_id",
+        "sysparm_limit": "1"
+    }
+    # Response code to be implemented
+    return None
